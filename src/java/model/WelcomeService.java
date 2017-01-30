2db99a612edaf10e8382cefa5d59a76384cafc92
amellem <amellem@L11105042790.wctc.msft>
2017-01-30 04:17:48 -0600

    final commit

--- src/java/model/WelcomeService.java
+++ src/java/model/WelcomeService.java
@@ -15,18 +15,8 @@ import java.util.Objects;
 public class WelcomeService {
     private final Calendar time = Calendar.getInstance();
     private final int hourOfDay = time.get(Calendar.HOUR_OF_DAY);
-    private String name;
     
-    public WelcomeService(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public WelcomeService() {
     }
     
     /**
@@ -49,45 +39,8 @@ public class WelcomeService {
      * @param name
      * @return concatenated greeting with the part of the day and the name
      */
-    public String greeting(String name){
+    public String doGreeting(String name){
         return "Good " + determinePartOfDay() + ", " + name + ". Welcome!";
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 13 * hash + Objects.hashCode(this.time);
-        hash = 13 * hash + this.hourOfDay;
-        hash = 13 * hash + Objects.hashCode(this.name);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final WelcomeService other = (WelcomeService) obj;
-        if (this.hourOfDay != other.hourOfDay) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return Objects.equals(this.time, other.time);
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-    
-    
-    
+  
 }
---
+++ web/result.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : result
+    Created on : Jan 30, 2017, 3:56:01 AM
+    Author     : CloudAerius
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Greetings</title>
+    </head>
+    <body>
+        <h1>${greeting}</h1>
+    </body>
+</html>
